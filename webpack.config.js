fb6f14ddee4006c9d77988e3bc5bd1ffc86a6f54
Mark Koo <koocool88@gmail.com>
2021-07-08 19:36:41 +0800

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -49,24 +49,6 @@ module.exports = {
                     },
                     darkMode: false,
                     theme: {
-                      aspectRatio: {
-                        1: "1",
-                        2: "2",
-                        3: "3",
-                        4: "4",
-                        5: "5",
-                        6: "6",
-                        7: "7",
-                        8: "8",
-                        9: "9",
-                        10: "10",
-                        11: "11",
-                        12: "12",
-                        13: "13",
-                        14: "14",
-                        15: "15",
-                        16: "16",
-                      },
                       extend: {
                         colors: {
                           primary: "#ff0000",
@@ -85,7 +67,6 @@ module.exports = {
                     },
                     plugins: [require("@tailwindcss/aspect-ratio")],
                   }),
-                  // require("postcss-css-variables"),
                   require("postcss-preset-env")({ stage: 1 }),
                   require("postcss-font-magician")({
                     display: "swap",
